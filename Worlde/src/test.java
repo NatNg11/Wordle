a520c02eef9894b74213274a31ee3387a6966dfb
Nathan <Nathan@NN.cfisd.net>
2022-04-11 13:46:17 -0500

    test

---
+++ Worlde/src/test.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.math.*;
+
+public class test {
+
+	public static void main(String[] args) throws Exception {
+		new test().run();
+	}
+
+	public void run() throws Exception {
+		//Scanner f = new Scanner(new File(".dat"));
+		Scanner f = new Scanner(System.in);
+		System.out.println("bruh");
+
+		f.close();
+	}
+}
